b5c95b342550e95eb97c01568b10fbf99ded98d9
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2020-04-19 17:29:51 +0430

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("salam");
         System.out.println("commit 2");
+        System.out.println("salam 3");
     }
 }
